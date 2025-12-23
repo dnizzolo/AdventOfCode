a4862715c8143b81edd7ae43218ee95d9d5f9a11
Daniele Nizzolo <dani.nizzolo@gmail.com>
2025-12-23 18:13:22 +0100

    Simplify `Main` module
    Current implementation was overkill and too rigid.
    It will have to change anyway if multiple years are to be supported.

--- app/Main.hs
+++ app/Main.hs
@@ -4,29 +4,18 @@ module Main (main) where
 
 import Runner (runDay)
 import System.Environment (getArgs, getProgName)
-import System.FilePath ((<.>), (</>))
 import System.IO (hPutStrLn, stderr)
 import Text.Read (readMaybe)
 
-usage :: String -> IO ()
-usage name = putStrLn $ "Usage: " <> name <> " <day> <example|input>"
-
-parseArgs :: (String, String) -> Either String (Int, FilePath)
-parseArgs (arg, mode) = case readMaybe @Int arg of
-  Just day -> case mode of
-    "example" -> Right (day, mkPath "examples" day)
-    "input" -> Right (day, mkPath "inputs" day)
-    _ -> Left $ "Unknown input kind: " <> mode
-  Nothing -> Left $ "Invalid day: " <> arg
- where
-  mkPath dir day = dir </> "day" <> leftPad (show day) <.> "txt"
-  leftPad day = replicate (2 - length day) '0' <> day
+usage :: IO ()
+usage = do
+  progName <- getProgName
+  putStrLn $ "Usage: " <> progName <> " <day> <input file>"
 
 main :: IO ()
-main = do
-  progName <- getProgName
+main =
   getArgs >>= \case
-    [arg, mode] -> case parseArgs (arg, mode) of
-      Left err -> hPutStrLn stderr err >> usage progName
-      Right (day, fileName) -> runDay fileName day
-    _ -> usage progName
+    [dayArg, fileName] -> case readMaybe @Int dayArg of
+      Just day -> runDay fileName day
+      Nothing -> hPutStrLn stderr ("Invalid day: " <> dayArg) >> usage
+    _ -> usage
